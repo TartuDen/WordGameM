74f7e39f18ad07e4c5a2f7c92a6d04fde395bf11
TartuDen <denver1033@gmail.com>
2025-03-14 22:05:51 +0200

    CLEANED CODE BEFORE UPDATES

--- mockData.js
+++ mockData.js
@@ -44,3 +44,22 @@ const englishWords = [
     }
   ];
 
+const user_data = {
+  user_id: "will be given by db, unique",
+  user_name: "Name, also unique",
+  user_reg_data: "time when user registered",
+  vocabulary:["general", "gaming"]
+}
+
+const user_progress = {
+  user_id: "",
+  guessed_words: [
+    {
+      word_id: 1, //word's id,
+      guess_correctly: 3,//number 
+      guessed_wrong: 1,
+    }
+  ]
+  
+
+}
